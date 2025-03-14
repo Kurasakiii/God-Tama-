4449713d827ff1a6803b2b513b31324c3b041dba
Kurasakiii <zoldyckryzz@gmail.com>
2025-03-14 09:55:04 +0700

    Tama Ganteng

---
+++ src/Quiz1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    alfabet()
+    movie()
+}
+
+fun alfabet(){
+    val initial='R'
+    println(initial)
+}
+
+fun movie(){
+    val nonton = mutableMapOf(
+        "Film 1" to " Five Hundred Days Of Summers",
+        "Film 2" to " Kung Fu Panda",
+        "Film 3" to " John Wick"
+    )
+    println(nonton)
+
+}
+
---
+++ src/Rawr.kt
@@ -0,0 +1,20 @@
+fun main() {
+    namaHewan()
+    namaMakanan()
+    namaHero()
+}
+fun namaHewan () {
+    val binatang = "Jerapah Makan "
+    print(binatang)
+}
+
+fun namaMakanan (){
+    val lapar = "Nasi Goreng Mankyoss Bersama "
+    print(lapar)
+}
+
+fun namaHero (){
+    val pake = "Hayabusa"
+    println(pake)
+}
+
